c4e6860e2b07761a2c3b2a0739a719ad3a0a2dd9
Alexander Knott <alexander.knott@r-kom.de>
2025-04-21 16:11:09 +0200

    fix: silence clippy warning
    Signed-off-by: Alexander Knott <alexander.knott@r-kom.de>

--- sapphire-core/src/build/formula/source/go.rs
+++ sapphire-core/src/build/formula/source/go.rs
@@ -70,14 +70,13 @@ pub fn go_build(
     cmd.arg("build");
     cmd.arg("-o");
     cmd.arg(&output_binary_path); // Output directly to install bin dir
-
-    // --- CORRECTED LDFLAGS ARGUMENTS ---
     cmd.arg("-ldflags"); // Pass the flag name as one argument
+    #[allow(clippy::suspicious_command_arg_space)] 
     cmd.arg("-s -w"); // Pass the flag value as the next argument
                       // --- END CORRECTION ---
 
-    // You might need to parse build_env for specific GOFLAGS or CGO flags here if needed
-    cmd.arg("."); // Build the package in the current directory (build_dir_dot)
+    // Might need to parse build_env for specific GOFLAGS or CGO flags here if needed
+    cmd.arg("."); // Build the package in the current directory
 
     // Apply the sanitized build environment (PATH, GOPATH, GOBIN etc. if set)
     build_env.apply_to_command(&mut cmd);
